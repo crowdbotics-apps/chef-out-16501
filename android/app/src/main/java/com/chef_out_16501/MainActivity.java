3ba03f447e2df66a769418d51560dae978f9d11b
Crowdbotics <team@crowdbotics.com>
2020-05-01 17:36:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chef_out_16501/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chef_out_16501;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "chef_out_16501";
+    }
+}
